fa7a16f650dd0b11f150bea1f59c75a42a251835
LyhourChhen <ukamilove66@gmail.com>
2020-03-23 22:54:34 +0700

    clean the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
-import React, { useState } from "react";
-import "./App.scss";
-import TodoList from "./components/TodoList";
-import AddTodoForm from "./components/AddTodoForm";
-import { Todo, ToggleTodo, AddTodo } from "./Interface/todo";
+import React from "react";
 
-const initialTodo: Array<Todo> = [
-  { text: "Researh", complete: true },
-  { text: "Hacking", complete: true }
-];
-
-const App: React.FC = () => {
-  const [todos, setTodos] = useState(initialTodo);
-
-  const toggleTodo: ToggleTodo = selectTodo => {
-    const updateTodo = todos.map(todo => {
-      if (todo === selectTodo) {
-        return {
-          ...todo,
-          complete: !todo.complete
-        };
-      }
-      return todo;
-    });
-    setTodos(updateTodo);
-  };
-
-  const addNewTodo: AddTodo = newTodo => {
-    setTodos([
-      ...todos,
-      {
-        text: newTodo,
-        complete: false
-      }
-    ]);
-  };
+const App = () => {
   return (
     <div>
-      <div style={{ flexDirection: "column" }} className="alwaysCenterScreen">
-        <h1>Todo React TypeScript</h1>
-        <AddTodoForm addNewTodo={addNewTodo} />
-        {todos.map(todo => (
-          <TodoList todo={todo} toggleTodo={toggleTodo} />
-        ))}
-      </div>
+      <h1>Hi</h1>
     </div>
   );
 };
--- src/Interface/todo.d.ts
+++
@@ -1,7 +0,0 @@
-export type Todo = {
-  text: String;
-  complete: boolean;
-};
-
-export type ToggleTodo = (selectTodo: Todo) => void;
-export type AddTodo = (newTodo: String) => void;
